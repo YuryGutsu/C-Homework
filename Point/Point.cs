7c76e4e96641e8d54854e013b6f3a25b6952c717
YuryGutsu <121096289+YuryGutsu@users.noreply.github.com>
2022-12-22 15:40:25 +0100

    Merge Point project
    * Add PupilsScore project
    
    * Add Point project
    
    * Code refactoring, delete unused Homework Program.cs

---
+++ Point/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointTask
+{
+    internal class Point
+    {
+        public string Name { get; }
+        public int CoordX { get; }
+        public int CoordY { get; }
+
+        public Point(string name, int coordX, int coordY)
+        {
+            Name = name;
+            CoordX = coordX;
+            CoordY = coordY;
+        }
+
+        public string ShowCoordinates()
+        {
+            return $"{Name} {CoordX},{CoordY}";    
+        }
+     
+        public string ShowDistance(Point point)
+        {
+           return $"{this.Name} - {point.Name} = {this.CalculateDistance(point)}";
+        }
+
+        private double CalculateDistance(Point point)
+        {
+            return Math.Sqrt((this.CoordX - point.CoordX) * (this.CoordX - point.CoordX)
+                             + (this.CoordY - point.CoordY) * (this.CoordY - point.CoordY));
+        }
+    }
+}

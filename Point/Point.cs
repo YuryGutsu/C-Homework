9ad895c0299baa7e42610040b466f72b5bf74be9
Yury Gutsu <yurgens99@gmail.com>
2022-12-22 10:46:36 +0100

    Add Point project

---
+++ Point/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointTask
+{
+    internal class Point
+    {
+        public string Name { get; }
+        public int CoordX { get; }
+        public int CoordY { get; }
+
+        public Point(string name, int coordX, int coordY)
+        {
+            Name = name;
+            CoordX = coordX;
+            CoordY = coordY;
+        }
+
+        public void ShowCoordinates()
+        {
+            Console.WriteLine($"{Name} {CoordX},{CoordY}");    
+        }
+     
+        public void ShowDistance(Point point)
+        {
+            Console.WriteLine($"{this.Name} - {point.Name} = {this.CalculateDistance(point)}");
+        }
+
+        private double CalculateDistance(Point point)
+        {
+            return Math.Sqrt((this.CoordX - point.CoordX) * (this.CoordX - point.CoordX)
+                             + (this.CoordY - point.CoordY) * (this.CoordY - point.CoordY));
+        }
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using PointTask;
+namespace PointTask
+{ 
+internal class Program
+{
+        static void Main(string[] args)
+        {
+            Point point1 = new("P1", 8, 12);
+            Point point2 = new("P2", 0, 8);
+            Point point3 = new("P3", 15, 45);
+            Point point4 = new("P4", -17, 33);
+            Point point5 = new("P5", 22, -5);
+            Point point6 = new("P6", 13, 18);
+            Point point7 = new("P7", -54, -75);
+            Point point8 = new("P8", 2, 22);
+            Point point9 = new("P9", -15, 5);
+            Point point10 = new("P10", 5, 5);
+
+            Point[] points = { point1, point2, point3, point4, point5, point6, point7, point8, point9, point10 };
+
+            foreach (Point point in points)
+            {
+                point.ShowCoordinates();
+            }
+
+            point5.ShowDistance(point6);
+            point9.ShowDistance(point10);
+        }
+
+}
+}

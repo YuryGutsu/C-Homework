5956c939a2d639771e01ac9867f750032bce41fd
Yury Gutsu <yurgens99@gmail.com>
2023-01-23 08:49:56 +0000

    select by TaxId and StaffDuty

--- UniversityEmployee/Program.cs
+++ UniversityEmployee/Program.cs
@@ -6,15 +6,17 @@ public class Program
     {
         var employee1 = new Teacher(new Person("Bob", "Dylan"), "AB1111", new Course("Math", "Algebra"));
         var employee2 = new Teacher(new Person("Mike", "Spencer"), "AB2222", new Course("History", "Ancient"));
-        var employee3 = new TeacherWithDegree(new Person("Sara", "White"), "AB3333", new Course("Biology", "Pathology"),
+        var employee3 = new Teacher(new Person("Dan", "Spencer"), "AB2223", new Course("Math", "Algebra"));
+        var employee4 = new Teacher(new Person("Andry", "Anderson"), "AB2224", new Course("Math", "Geometry"));
+        var employee5 = new TeacherWithDegree(new Person("Sara", "White"), "AB3333", new Course("Biology", "Pathology"),
                                                                                             "Magistr", "Rector");
-        var employee4 = new SupportStaff(new Person("Dory", "Minny"), "GG1111", "Guard");
-        var employee5 = new SupportStaff(new Person("Serge", "McNill"), "GG2222", "Janitor");
-        var employee6 = new SupportStaff(new Person("Ben", "Brown"), "GG3333", "Gardener");
+        var employee6 = new SupportStaff(new Person("Dory", "Minny"), "GG1111", "Guard");
+        var employee7 = new SupportStaff(new Person("Serge", "McNill"), "GG2222", "Janitor");
+        var employee8 = new SupportStaff(new Person("Ben", "Brown"), "GG3333", "Gardener");
         var employeeRector = new Rector(new Person("Taras", "Bulba"), "RR1111", "BGUIR");
       
         var employees = new List<UniversityEmployee>() { employee1, employee2, employee3,employee4,
-                                                         employee5, employee6, employeeRector };
+                                                         employee5, employee6, employee7, employee8, employeeRector };
         var Building1 = new Building(new List<Room>() {new Room(RoomType.LectionRoom, 1),
                                                           new Room(RoomType.LaboratoryRoom, 2),
                                                           new Room(RoomType.SeminarRoom, 3),
@@ -37,15 +39,36 @@ public class Program
         Console.WriteLine(universityBguir.AddEmployee(employee5));
         Console.WriteLine();
 
-        var test = universityBguir.Employees.Where(x => x.Person.LastName.StartsWith('B')).OrderBy(x => x.TaxId).ToList();
+        var filterEmployee1 = universityBguir.Employees.Where(x => x.Person.LastName.StartsWith('B'))
+                                                      .OrderBy(x => x.TaxId).ToList();
 
-        foreach (var item in test)
+        foreach (var item in filterEmployee1)
         {
             Console.WriteLine(item.GetOfficialDuties());
         }
 
         Console.WriteLine();
-       
+
+        var filterEmployee2 = universityBguir.Employees
+            .Where(x => (x is Teacher teacher) && teacher.Course.CourseName.Equals("Math")).ToList();
+
+        foreach (var item in filterEmployee2)
+        {
+            Console.WriteLine(item.GetOfficialDuties());
+        }
+
+        Console.WriteLine();
+
+        var filterEmployee3 = universityBguir.Employees
+            .Where(x => (x is SupportStaff))
+            .Select(supportstaff => (supportstaff.TaxId, ((SupportStaff) supportstaff).StaffDuty)).ToList();
+
+        foreach (var item in filterEmployee3)
+        {
+            Console.WriteLine("TaxId = {0}, StaffDuty = {1}", item.TaxId, item.StaffDuty);
+        }
+
+        Console.WriteLine();
 
         foreach (var employee in employees)
         {
